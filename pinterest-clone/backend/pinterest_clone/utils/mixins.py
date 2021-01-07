5b0e48a89ee7e762cb081c53a24c4033fe7f3d6a
Ricardo Ramos <rcdo.ramos@gmail.com>
2021-01-07 16:30:10 -0300

    Cleanup

--- pinterest-clone/backend/pinterest_clone/utils/mixins.py
+++
@@ -1,8 +0,0 @@
-from django.db.models import Model, DateTimeField
-
-class TimeStampMixin(Model):
-    created_at = DateTimeField(auto_now_add=True)
-    updated_at = DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True
